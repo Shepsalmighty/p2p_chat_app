54e97c47ae10edfb28dab9c0fe891450eac14ee2
Sheps <shepsalmighty@gmail.com>
2024-03-20 20:13:09 +0000

    wrap client + server code in functions

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
 import socket
 
+
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    message = input("Enter message: ")
-    s.sendall(message.encode("utf-32"))
-    data = s.recv(1024)
+def send(HOST, PORT):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        message = input("Enter message: ")
+        # s.sendall(message.encode("utf-32"))
+        data = s.recv(1024)
+        while data:
+            s.sendall(message.encode("utf-32"))
+            print(f"Received {data!r}")
 
-print(f"Received {data!r}")
+
+# print(f"Received {data!r}")
+
+if __name__ == "__main__":
+    send(HOST, PORT)
--- server.py
+++ server.py
@@ -6,15 +6,20 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(data.decode("utf-32"))
-            conn.sendall(data)
+def recieve(HOST, PORT):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data.decode("utf-32"))
+                conn.sendall(data)
+
+
+if __name__ == "__main__":
+    recieve(HOST,PORT)
