dad4f4e5e68229ab36ea7f4c6bb1e3b3d0757ea6
Sheps <shepsalmighty@gmail.com>
2024-03-21 19:32:52 +0000

    fix client side while loop

--- client.py
+++ client.py
@@ -1,21 +1,27 @@
 import socket
+# import server
 
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+SERVER_HOST = "127.0.0.1"  # The server's hostname or IP address
+SERVER_PORT = 65432  # The port used by the server
 
-def send(HOST, PORT):
+def send(SERVER_HOST, SERVER_PORT):
+    # creating client socket as s to connect to server, using TCP/IP -- IP = (AF_INET) via TCP = (SOCK_STREAM))
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        message = input("Enter message: ")
-        # s.sendall(message.encode("utf-32"))
-        data = s.recv(1024)
-        while data:
+        s.connect((SERVER_HOST, SERVER_PORT))
+        # data = s.recv(1024)
+
+        #maintaining server connection while messages are sent "bye" to exit the loop
+        while True:
+            message = input("Enter message: ")
             s.sendall(message.encode("utf-32"))
-            print(f"Received {data!r}")
+            if message.lower() == "bye":
+                break
 
 
-# print(f"Received {data!r}")
+
+
 
 if __name__ == "__main__":
-    send(HOST, PORT)
+    send(SERVER_HOST, SERVER_PORT)
+
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+# import client
 
 
 # sock = socket.sock_STREAM
@@ -6,20 +7,32 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+
 def recieve(HOST, PORT):
+    # creating server socket as s via TCP/IP
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # binds the socket to the specified port and IP
         s.bind((HOST, PORT))
+
+        # instructing the server to listen for connection requests (breaks if no connection)
         s.listen()
+
+        # assigns the server socket to conn and client IP and Port to addr. s.accept() completes the handshake
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
+            print(f"let's see what conn is:  {conn}")
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
-                print(data.decode("utf-32"))
-                conn.sendall(data)
+                data.decode("utf-32")
+                # conn.sendall(data)
+                print(data.decode('utf-32'))
+
+
+
 
 
 if __name__ == "__main__":
-    recieve(HOST,PORT)
+    recieve(HOST, PORT)
