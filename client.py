73fa393ae44d3037359c872cb564f015e4301115
Sheps <shepsalmighty@gmail.com>
2024-03-22 19:19:23 +0000

    implement threading for server + client send/receive functions

--- client.py
+++ client.py
@@ -1,27 +1,47 @@
 import socket
 # import server
+import threading
 
 
 SERVER_HOST = "127.0.0.1"  # The server's hostname or IP address
 SERVER_PORT = 65432  # The port used by the server
 
-def send(SERVER_HOST, SERVER_PORT):
+
+# define sender() and receiver() to be used locally in client_main() where we can pass s obj
+def sender(s):
+    while True:
+        message = input("Enter message: ")
+        s.sendall(message.encode("utf-32"))
+        if message.lower() == "bye":
+            break
+def receiver(s):
+    while True:
+        data = s.recv(1024)
+        if not data:
+            break
+        print(f"Recieved: {data.decode("utf-32")}")
+
+def client_main(SERVER_HOST, SERVER_PORT):
     # creating client socket as s to connect to server, using TCP/IP -- IP = (AF_INET) via TCP = (SOCK_STREAM))
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((SERVER_HOST, SERVER_PORT))
         # data = s.recv(1024)
 
-        #maintaining server connection while messages are sent "bye" to exit the loop
-        while True:
-            message = input("Enter message: ")
-            s.sendall(message.encode("utf-32"))
-            if message.lower() == "bye":
-                break
+#maintaining server connection while messages are sent "bye" to exit the loop
+ # create 2 thread instances to run the sender + receiver functions concurrently
+        sender_thread = threading.Thread(target=sender, args=(s,))
+        receiver_thread = threading.Thread(target=receiver, args=(s,))
 
+        # .start calls the functions on respective threads
+        sender_thread.start()
+        receiver_thread.start()
 
+        # .join breaks and rejoins the threads, code now runs sequentially
+        sender_thread.join()
+        receiver_thread.join()
 
 
 
 if __name__ == "__main__":
-    send(SERVER_HOST, SERVER_PORT)
+    client_main(SERVER_HOST, SERVER_PORT)
 
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 # import client
+import threading
 
 
 # sock = socket.sock_STREAM
@@ -7,8 +8,23 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+# define sender() + reciever() to be called locally in server_main where we can pass conn obj
+def sender(conn):
+     while True:
+        message = input("Enter message: ")
+        conn.sendall(message.encode("utf-32"))
+        if message.lower() == "bye":
+            break
 
-def recieve(HOST, PORT):
+def receiver(conn):
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(f"Received: {data.decode("utf-32")}")
+
+
+def server_main(HOST, PORT):
     # creating server socket as s via TCP/IP
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # binds the socket to the specified port and IP
@@ -17,22 +33,26 @@ def recieve(HOST, PORT):
         # instructing the server to listen for connection requests (breaks if no connection)
         s.listen()
 
-        # assigns the server socket to conn and client IP and Port to addr. s.accept() completes the handshake
+        # assigns the server socket to conn and client IP + Port to addr. s.accept() completes the handshake
         conn, addr = s.accept()
         with conn:
             print(f"Connected by {addr}")
             print(f"let's see what conn is:  {conn}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                data.decode("utf-32")
-                # conn.sendall(data)
-                print(data.decode('utf-32'))
 
+            # create 2 thread instances to run the sender + receiver functions concurrently
+            sender_thread = threading.Thread(target=sender, args=(conn,))
+            receiver_thread = threading.Thread(target=receiver, args=(conn,))
+
+            # .start calls the functions on respective threads
+            sender_thread.start()
+            receiver_thread.start()
+
+            # .join breaks and rejoins the threads, code now runs sequentially
+            sender_thread.join()
+            receiver_thread.join()
 
 
 
 
 if __name__ == "__main__":
-    recieve(HOST, PORT)
+    server_main(HOST, PORT)
