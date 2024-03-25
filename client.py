a32a860ddd1f79a4c927749079877f3bc3172e4f
Sheps <shepsalmighty@gmail.com>
2024-03-25 19:39:15 +0000

    add gui and quality of life updates

--- client.py
+++ client.py
@@ -1,25 +1,57 @@
 import socket
-# import server
 import threading
+from tkinter import *
 
-
+# hardcoding server TCP/IP
 SERVER_HOST = "127.0.0.1"  # The server's hostname or IP address
 SERVER_PORT = 65432  # The port used by the server
 
+# gui code shtolen  from https://www.geeksforgeeks.org/gui-chat-application-using-tkinter-in-python/
+
+# GUI
+root = Tk()
+root.title("client")
+
+BG_GRAY = "#ABB2B9"
+BG_COLOR = "#17202A"
+TEXT_COLOR = "#EAECEE"
+
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
+
+
+
+lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10, width=20, height=1).grid(
+	row=0)
+
+txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
+txt.grid(row=1, column=0, columnspan=2)
+
+scrollbar = Scrollbar(txt)
+scrollbar.place(relheight=1, relx=0.974)
+
+e = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
+e.grid(row=2, column=0)
+
+# send = Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
+# 			command=sender).grid(row=2, column=1)
+
+
+
 
 # define sender() and receiver() to be used locally in client_main() where we can pass s obj
-def sender(s):
-    while True:
-        message = input("Enter message: ")
-        s.sendall(message.encode("utf-32"))
-        if message.lower() == "bye":
-            break
+# def sender(s):
+#     while True:
+#         message = input("Enter message: ")
+#         s.sendall(message.encode("utf-32"))
+#         if message.lower() == "bye":
+#             break
 def receiver(s):
     while True:
         data = s.recv(1024)
         if not data:
             break
-        print(f"Recieved: {data.decode("utf-32")}")
+        txt.insert(END, "\n" + f"Received: {data.decode("utf-32")}")
 
 def client_main(SERVER_HOST, SERVER_PORT):
     # creating client socket as s to connect to server, using TCP/IP -- IP = (AF_INET) via TCP = (SOCK_STREAM))
@@ -27,18 +59,28 @@ def client_main(SERVER_HOST, SERVER_PORT):
         s.connect((SERVER_HOST, SERVER_PORT))
         # data = s.recv(1024)
 
+        def sender():
+            if s:
+                message = "Sent: " + e.get()
+                txt.insert(END, "\n" + message)
+                s.sendall(e.get().encode("utf-32"))
+                e.delete(0, END)
+
+
+
+        Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
+               command=sender).grid(row=2, column=1)
 #maintaining server connection while messages are sent "bye" to exit the loop
  # create 2 thread instances to run the sender + receiver functions concurrently
-        sender_thread = threading.Thread(target=sender, args=(s,))
+ #        sender_thread = threading.Thread(target=sender, args=(s,))
         receiver_thread = threading.Thread(target=receiver, args=(s,))
-
-        # .start calls the functions on respective threads
-        sender_thread.start()
+ #
+ #        # .start calls the functions on respective threads
+ #        sender_thread.start()
         receiver_thread.start()
 
-        # .join breaks and rejoins the threads, code now runs sequentially
-        sender_thread.join()
-        receiver_thread.join()
+        # opens chat window, closing window ends program
+        root.mainloop()
 
 
 
--- server.py
+++ server.py
@@ -1,6 +1,34 @@
 import socket
-# import client
 import threading
+
+from tkinter import *
+
+# gui code shtolen  from https://www.geeksforgeeks.org/gui-chat-application-using-tkinter-in-python/
+
+# GUI
+root = Tk()
+root.title("server")
+
+BG_GRAY = "#ABB2B9"
+BG_COLOR = "#17202A"
+TEXT_COLOR = "#EAECEE"
+
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
+
+
+lable1 = Label(root, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10, width=20, height=1).grid(
+	row=0)
+
+txt = Text(root, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=60)
+txt.grid(row=1, column=0, columnspan=2)
+
+scrollbar = Scrollbar(txt)
+scrollbar.place(relheight=1, relx=0.974)
+
+e = Entry(root, bg="#2C3E50", fg=TEXT_COLOR, font=FONT, width=55)
+e.grid(row=2, column=0)
+
 
 
 # sock = socket.sock_STREAM
@@ -9,19 +37,25 @@ HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 # define sender() + reciever() to be called locally in server_main where we can pass conn obj
-def sender(conn):
-     while True:
-        message = input("Enter message: ")
-        conn.sendall(message.encode("utf-32"))
-        if message.lower() == "bye":
-            break
+# def sender(conn):
+#     while True:
+#         message = input("Enter message: ")
+#         conn.sendall(message.encode("utf-32"))
+#
+#         if message.lower() == "bye":
+#             break
+#         return conn.sendall(message.encode("utf-32"))
+
+
+
+
 
 def receiver(conn):
     while True:
         data = conn.recv(1024)
         if not data:
             break
-        print(f"Received: {data.decode("utf-32")}")
+        txt.insert(END, "\n" + f"Received: {data.decode("utf-32")}")
 
 
 def server_main(HOST, PORT):
@@ -33,23 +67,40 @@ def server_main(HOST, PORT):
         # instructing the server to listen for connection requests (breaks if no connection)
         s.listen()
 
-        # assigns the server socket to conn and client IP + Port to addr. s.accept() completes the handshake
+
+
+        #assigns the server socket to conn and client IP + Port to addr. s.accept() completes the handshake
         conn, addr = s.accept()
+
+
+
         with conn:
             print(f"Connected by {addr}")
             print(f"let's see what conn is:  {conn}")
 
+            def sender():
+                if conn:
+                    message = "Sent: " + e.get()
+                    txt.insert(END, "\n" + message)
+                    conn.sendall(e.get().encode("utf-32"))
+                    e.delete(0, END)
+
+
             # create 2 thread instances to run the sender + receiver functions concurrently
-            sender_thread = threading.Thread(target=sender, args=(conn,))
+            Button(root, text="Send", font=FONT_BOLD, bg=BG_GRAY,
+                          command=sender).grid(row=2, column=1)
+            # sender_thread = threading.Thread(target=sender, args=(conn,))
             receiver_thread = threading.Thread(target=receiver, args=(conn,))
 
             # .start calls the functions on respective threads
-            sender_thread.start()
+            # sender_thread.start()
             receiver_thread.start()
 
-            # .join breaks and rejoins the threads, code now runs sequentially
-            sender_thread.join()
-            receiver_thread.join()
+            # receiver_thread.join()
+
+            # opens chat window, closing window ends program
+            root.mainloop()
+
 
 
 
